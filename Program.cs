3df1c62cae0acdd1f3d9e11e1a66dda3b23200d3
AlejandroBlandin <zb0601042019@unab.edu.sv>
2023-02-25 22:07:47 -0600

    cambios

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ingresamos el valor inicial
+Console.WriteLine("Ingrese una valor");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Ingresamos el valor final
+Console.WriteLine("Ingrese otro valor");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+//Iteramos desde el valor inicial hasta el valor final
+Console.Write("Los números pares son: ");
+for (int i = num1; i <= num2; i++)
+{
+    //Verificamos si el número es par
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
